c94b38cfd6a6db16845a9b00626a14e5283e68c1
webstriix <webstriix@gmail.com>
2025-01-10 16:52:35 +0700

    create NewsCategories and NewsWithCategories model

--- src/models/news.rs
+++ src/models/news.rs
@@ -1,4 +1,5 @@
 use crate::schema::news;
+use crate::schema::news_categories;
 use diesel::prelude::{Insertable, Queryable};
 use serde::{Deserialize, Serialize};
 
@@ -11,4 +12,19 @@ pub struct News {
     pub author_id: i32,
     pub created_at: chrono::NaiveDateTime,
     pub updated_at: chrono::NaiveDateTime,
+}
+
+#[derive(Insertable, Queryable, Serialize, Deserialize)]
+#[table_name = "news_categories"]
+pub struct NewsCategory {
+    pub news_id: i32,
+    pub category_id: i32,
+}
+
+#[derive(Queryable, Serialize, Deserialize)]
+pub struct NewsWithCategories {
+    pub id: i32,
+    pub title: String,
+    pub content: String,
+    pub categories: Vec<String>
 }
