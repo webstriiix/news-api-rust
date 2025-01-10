d574cc3818a0ffc1ed362b40991fd29caab4236d
webstriix <webstriix@gmail.com>
2025-01-10 16:51:35 +0700

    make it insertable

--- src/models/category.rs
+++ src/models/category.rs
@@ -1,11 +1,13 @@
-use diesel::prelude::Queryable;
+use diesel::prelude::{Insertable, Queryable};
+use crate::schema::categories;
 use serde::{Deserialize, Serialize};
 
-#[derive(Queryable, Serialize, Deserialize)]
+#[derive(Queryable, Serialize, Deserialize, Insertable)]
+#[table_name = "categories"]
 pub struct Category {
     pub id: i32,
     pub name: String,
-    pub describtion: Option<String>,
+    pub description: Option<String>,
     pub created_at: chrono::NaiveDateTime,
     pub updated_at: chrono::NaiveDateTime,
 }
