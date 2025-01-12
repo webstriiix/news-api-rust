b844a1bf0e97d65f67063fd09edbf5a180b2d807
webstriix <webstriix@gmail.com>
2025-01-12 22:57:39 +0700

    create new user struct
    separate the user and new user struct for API safety

--- src/models/user.rs
+++ src/models/user.rs
@@ -1,4 +1,5 @@
-use diesel::prelude::Queryable;
+use crate::schema::users;
+use diesel::prelude::{Insertable, Queryable};
 use serde::{Deserialize, Serialize};
 
 #[derive(Queryable, Serialize, Deserialize)]
@@ -9,4 +10,12 @@ pub struct User {
     pub is_admin: bool,
     pub created_at: chrono::NaiveDateTime,
     pub updated_at: chrono::NaiveDateTime,
+}
+
+#[derive(Queryable, Insertable, Serialize, Deserialize)]
+#[table_name = "users"]
+pub struct NewUser {
+    pub username: String,
+    pub password: String,
+    pub is_admin: bool,
 }
