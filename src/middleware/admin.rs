a423377f0bed772d26234f8f66ef8be602a444f5
webstriix <webstriix@gmail.com>
2025-01-16 23:51:44 +0700

    create admin middleware to check if user is admin

---
+++ src/middleware/admin.rs
@@ -0,0 +1,61 @@
+use actix_web::dev::{forward_ready, Service, ServiceRequest, ServiceResponse, Transform};
+use actix_web::{Error, HttpMessage};
+use futures::future::{ready, LocalBoxFuture, Ready};
+use jsonwebtoken::{decode, DecodingKey, Validation};
+use std::task::{Context, Poll};
+
+use crate::utils::jwt::Claims;
+
+pub struct AdminMiddleware;
+
+impl<S, B> Transform<S, ServiceRequest> for AdminMiddleware
+where
+    S: Service<ServiceRequest, Response = ServiceResponse<B>, Error = Error>,
+    S::Future: 'static,
+    B: 'static,
+{
+    type Response = ServiceResponse<B>;
+    type Error = Error;
+    type InitError = ();
+    type Transform = AdminMiddlewareService<S>;
+    type Future = Ready<Result<Self::Transform, Self::InitError>>;
+
+    fn new_transform(&self, service: S) -> Self::Future {
+        ready(Ok(AdminMiddlewareService { service }))
+    }
+}
+
+pub struct AdminMiddlewareService<S> {
+    service: S,
+}
+
+impl<S, B> Service<ServiceRequest> for AdminMiddlewareService<S>
+where
+    S: Service<ServiceRequest, Response = ServiceResponse<B>, Error = Error>,
+    S::Future: 'static,
+    B: 'static,
+{
+    type Response = ServiceResponse<B>;
+    type Error = Error;
+    type Future = LocalBoxFuture<'static, Result<Self::Response, Self::Error>>;
+
+    forward_ready!(service);
+
+    fn call(&self, req: ServiceRequest) -> Self::Future {
+        let is_admin = req.extensions().get::<Claims>()
+                    .map(|claims| claims.is_admin)
+                    .unwrap_or(false);
+
+        if is_admin {
+            let fut = self.service.call(req);
+            Box::pin(async move {
+                let res = fut.await?;
+                Ok(res)
+            })
+        } else {
+            Box::pin(ready(Err(actix_web::error::ErrorForbidden(
+                "Admin access required",
+            ))))
+        }    
+    }
+}
