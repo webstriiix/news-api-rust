20cb5c3359be3059b11aae5f11de17a4e4819ec8
webstriix <webstriix@gmail.com>
2025-01-26 17:39:54 +0700

    add validate and use diesel table_name

--- src/models/category.rs
+++ src/models/category.rs
@@ -1,12 +1,15 @@
 use crate::schema::categories;
-use diesel::prelude::{Insertable, Queryable};
+use diesel::prelude::{AsChangeset, Insertable, Queryable};
 use serde::{Deserialize, Serialize};
+use validator::Validate;
 
-#[derive(Queryable, Serialize, Deserialize, Insertable)]
+#[derive(Debug, Queryable, Serialize, Deserialize, Insertable, Validate)]
 #[table_name = "categories"]
 pub struct Category {
     pub id: i32,
+    #[validate(length(min = 1, message = "Name is required"))]
     pub name: String,
+    #[validate(length(min = 1, message = "Description is required"))]
     pub description: Option<String>,
     pub created_at: chrono::NaiveDateTime,
     pub updated_at: chrono::NaiveDateTime,
@@ -20,9 +23,31 @@ pub struct CategorySummary {
 
 #[derive(Debug, Deserialize, Validate)]
 pub struct UpdateCategoryRequest {
-    #[validate(length(min = 3, max = 100, message = "Category name must be between 3 and 100 characters!"))]
+    #[validate(length(
+        min = 3,
+        max = 100,
+        message = "Category name must be between 3 and 100 characters!"
+    ))]
     pub name: String,
 
-    #[validate(length(min = 3, max = 255, message = "Category description must be between 3 and 255 characters!"))]
+    #[validate(length(
+        min = 3,
+        max = 255,
+        message = "Category description must be between 3 and 255 characters!"
+    ))]
     pub description: String,
+}
+
+#[derive(AsChangeset)]
+#[diesel(table_name = categories)]
+pub struct CategoryChangeset {
+    pub name: Option<String>,        // Use Option<String> for optional updates
+    pub description: Option<String>, // Use Option<String> for optional updates
+    pub updated_at: chrono::NaiveDateTime,
+}
+
+#[derive(Debug, Serialize)]
+pub struct UpdateCategoryResponse {
+    pub message: String,
+    pub category: Category,
 }
