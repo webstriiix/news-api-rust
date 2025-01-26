23b603827791dc5a6f54f9ebe0d293b8502c749a
webstriix <webstriix@gmail.com>
2025-01-26 17:42:14 +0700

    add the update_category function to the routes

--- src/routes.rs
+++ src/routes.rs
@@ -1,6 +1,9 @@
 use actix_web::web;
 
-use crate::{handlers::admin::update_news, middleware::{admin::AdminMiddleware, auth::AuthMiddleWare}};
+use crate::{
+    handlers::admin::update_news,
+    middleware::{admin::AdminMiddleware, auth::AuthMiddleWare},
+};
 
 pub fn configure_routes(cfg: &mut web::ServiceConfig) {
     // Admin routes
@@ -17,24 +20,45 @@ pub fn configure_routes(cfg: &mut web::ServiceConfig) {
                 "/list-news",
                 web::get().to(crate::handlers::news::list_news),
             )
-            .route("/create-category", web::post().to(crate::handlers::admin::create_category))
-            .route("/news-detail/{id}", web::get().to(crate::handlers::news::get_news_detail))
-            .route("/delete-news/{id}", web::get().to(crate::handlers::admin::delete_news))
-            .route("/delete-category/{id}", web::get().to(crate::handlers::admin::delete_category)),
+            .route(
+                "/create-category",
+                web::post().to(crate::handlers::admin::create_category),
+            )
+            .route(
+                "/create-category",
+                web::post().to(crate::handlers::admin::update_category),
+            )
+            .route(
+                "/news-detail/{id}",
+                web::get().to(crate::handlers::news::get_news_detail),
+            )
+            .route(
+                "/delete-news/{id}",
+                web::get().to(crate::handlers::admin::delete_news),
+            )
+            .route(
+                "/delete-category/{id}",
+                web::get().to(crate::handlers::admin::delete_category),
+            ),
     );
 
     // Auth route
-    cfg.service(web::scope("/auth") 
+    cfg.service(
+        web::scope("/auth")
             .route("/register", web::post().to(crate::handlers::auth::register))
             .route("/login", web::post().to(crate::handlers::auth::login)),
-        );
+    );
 
     // User routes
-    cfg.service(web::scope("/user")
-        .route(
-            "/list-news",
-            web::get().to(crate::handlers::news::list_news),
-        )
-        .route("/create-category", web::post().to(crate::handlers::admin::create_category))
+    cfg.service(
+        web::scope("/user")
+            .route(
+                "/list-news",
+                web::get().to(crate::handlers::news::list_news),
+            )
+            .route(
+                "/create-category",
+                web::post().to(crate::handlers::admin::create_category),
+            ),
     );
 }
