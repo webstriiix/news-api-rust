58d9c7402d5b0b1ef0346b117ed7765dd8c56e44
webstriix <webstriix@gmail.com>
2025-02-03 18:30:18 +0700

    import NewsSummary struct

--- src/handlers/news.rs
+++ src/handlers/news.rs
@@ -5,16 +5,9 @@ use serde::Serialize;
 
 use crate::db::DBPool;
 use crate::models::category::CategorySummary;
-use crate::models::news::{News, NewsDetail};
+use crate::models::news::{News, NewsDetail, NewsSummary};
 use crate::schema::news::dsl::*;
 use crate::schema::{categories, news_categories};
-
-// struct for json response list_news
-#[derive(Serialize)]
-struct NewsSummary {
-    title: String,
-    created_at: chrono::NaiveDateTime,
-}
 
 pub async fn list_news(pool: web::Data<DBPool>) -> HttpResponse {
     let mut conn = match pool.get() {
